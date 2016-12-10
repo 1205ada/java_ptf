8d0a54bd1743dba42e7f3ca637fa3f2a58ce0a38
1205ada <aadriannajuda@gmail.com>
2016-12-10 15:36:22 +0100

    Dodano klasę Distance1 i Point - Homework no. 2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Ada on 2016-12-07.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+  public double area (){
+    return this.l * this.l;
+  }
+
+}
