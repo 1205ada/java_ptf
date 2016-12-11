da752f4619de1488b30715cfcc04909050b6980e
1205ada <aadriannajuda@gmail.com>
2016-12-11 15:48:52 +0100

    Dodanie dodatkowej klasy Square

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Ada on 2016-12-11.
+ */
+public class Square {
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+  public double area (){
+    return this.l * this.l;
+  }
+}
