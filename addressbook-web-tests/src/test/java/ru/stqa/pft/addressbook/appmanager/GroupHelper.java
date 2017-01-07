1b0bababca6b19d0786ca6809824d90e10596682
1205ada <aadriannajuda@gmail.com>
2017-01-07 18:04:57 +0100

    Collection creating
    Kształtowanie kolekcji

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,8 +2,12 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Ada on 2016-12-17.
@@ -39,7 +43,7 @@ public class GroupHelper extends HelperBase{
   public void selectGroup(int index) {
     wd.findElements(By.name("selected[]")).get(index).click();
   }
-  
+
   public void initGroupModification() {
     click(By.name("edit"));
   }
@@ -61,5 +65,16 @@ public class GroupHelper extends HelperBase{
 
   public int getGroupCount() {
     return wd.findElements(By.name("selected[]")).size();
+  }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for (WebElement element : elements) {
+      String name = element.getText();
+      GroupData group = new GroupData(name, null,  null);
+      groups.add(group);
+    }
+    return groups;
   }
 }
