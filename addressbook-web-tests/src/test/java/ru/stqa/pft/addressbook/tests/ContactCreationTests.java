c1bd2c36304405f54ea175a6e833f7aaa9216970
1205ada <aadriannajuda@gmail.com>
2017-02-12 18:25:42 +0100

    Homework no, 18 commit 5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -6,14 +6,70 @@ import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.io.File;
+public class ContactCreationTests extends TestBase {
+  int id;
 
-
-
+ /* @DataProvider
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")))) {
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xstream = new XStream();
+      xstream.processAnnotations(ContactData.class);
+      List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+      return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
+  }
 
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))) {
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+      }.getType()); //List<GContactData>.class
+      return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
+  }*/
 
-
+  @Test//(dataProvider = "validContactsFromXml")
+  public void ContactCreationTests() {
+    if (app.db().groups().size() == 0) {
+      GroupData group = new GroupData().withName("test 1").withHeader("header 1").withFooter("footer 1");
+      app.group().create(group);
     }
+    Groups groups = app.db().groups();
+    GroupData group = groups.iterator().next();
+    String nameGroup = group.getName();
+    Contacts contacts = app.db().contacts();
+    ContactData newContact = new ContactData().withFirstname("Anna").withLastname("Kowalska").inGroup(groups.iterator().next());
+    //File photo = new File("src/test/resources/stru.jpg");
+
+    Contacts before = app.contact().all();
+    //ContactData contact = new ContactData("Anna", "Kowalska"/*, "Katarzyna", "akowalska", "Krzemowa 3/2\n32-900 Katowice", "akowalska@gmail.com", "test1"*/);
+
+    for (ContactData contact : contacts) {
+      id = contact.getId();
+    }
+    System.out.println("ID = " + id);
+    app.contact().create(newContact);
+    app.contact().selectContactById(newContact.getId());
+    app.contact().selectGroup(nameGroup);
+    app.contact().addToGroup();
+    //assertThat(app.contact().count(), equalTo(before.size() + 1));
+    //Contacts after = app.contact().all();
+    //assertEquals(after.size(), before.size() +1);
+    //assertThat(after, equalTo(
+    // before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    verifyContactListInUI();
+  }
 }
